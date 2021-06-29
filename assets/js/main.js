e769a5dbbacbe65f55be40a3990bbc91012da19f
Saravanan <saravananvsmls2018@gmail.com>
2021-06-29 18:16:06 +0530

    Changes are commited

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,25 +93,6 @@
   }, {
     offset: '80%'
   });
-
-  // Porfolio isotope and filter
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
-    });
-
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-    });
-
-  });
-
   // Initiate venobox (lightbox feature used in portofilo)
   $(document).ready(function() {
     $('.venobox').venobox();
